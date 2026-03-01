1b8b2cedc30d84cad5693f3522fd71beb2975b36
Jelius Basumatary <personal@jelius.dev>
2026-03-01 13:27:51 +0530

    Implemented some boiler plate code for API

--- api/handler/error.go
+++ api/handler/error.go
@@ -2,6 +2,7 @@ package handler
 
 import (
     "encoding/json"
+    "fmt"
     "mandana/types"
     "net/http"
 
@@ -10,12 +11,22 @@ import (
 
 func HttpError(w http.ResponseWriter, err error, code int) {
     w.Header().Set("Content-Type", "application/json")
+
     if err = json.NewEncoder(w).Encode(types.HTTPError{
         Code:    code,
         Error:   http.StatusText(code),
         Message: err.Error(),
     }); err != nil {
         logger.Panic("Failed to encode http error:", err.Error())
+    }
+}
+
+func HTTPPlaceholder(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusNotImplemented)
+
+    if _, err := fmt.Fprintf(w, `{"code":"501", "error": "Not Implemented", "message": "TODO: Implement %c%s%c route."}`, '`', r.URL.Path, '`'); err != nil {
+        logger.Panic("Failed to write http response:", err.Error())
     }
 }
 
--- api/router.go
+++ api/router.go
@@ -1,14 +1,74 @@
 package api
 
 import (
+    "fmt"
     "mandana/api/handler"
     "net/http"
+    "strings"
+
+    "github.com/jelius-sama/logger"
 )
+
+type HTTPMethod int
+
+const (
+    MethodGET HTTPMethod = iota
+    MethodPOST
+    MethodPATCH
+    MethodPUT
+    MethodDELETE
+)
+
+func (hm HTTPMethod) String() string {
+    switch hm {
+    case MethodGET:
+        return "GET"
+    case MethodPOST:
+        return "POST"
+    case MethodPATCH:
+        return "PATCH"
+    case MethodPUT:
+        return "PUT"
+    case MethodDELETE:
+        return "DELETE"
+    }
+
+    // TODO: Handle other HTTP Methods
+    logger.Panic("Unreachable")
+    return "GET"
+}
+
+// Generates and returns an absolute path
+func absPath(path string, method HTTPMethod) string {
+    return fmt.Sprintf(
+        "%s /api/%s/{$}",
+        method,
+        func() string {
+            var cleaned, _ = strings.CutPrefix(path, "/")
+            return cleaned
+        }(),
+    )
+}
+
+// Generates and returns a generic path
+func genPath(path string, method HTTPMethod) string {
+    return fmt.Sprintf(
+        "%s /api/%s/",
+        method,
+        func() string {
+            var cleaned, _ = strings.CutPrefix(path, "/")
+            return cleaned
+        }(),
+    )
+}
 
 func Router() *http.ServeMux {
     var mux *http.ServeMux = http.NewServeMux()
 
-    mux.HandleFunc("GET /api/stats/{$}", handler.HandleStats)
+    mux.HandleFunc(absPath("/get/all", MethodGET), handler.HTTPPlaceholder)
+    mux.HandleFunc(genPath("get", MethodGET), handler.HTTPPlaceholder)
+
+    mux.HandleFunc(absPath("stats", MethodGET), handler.HandleStats)
 
     return mux
 }

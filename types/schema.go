57105a2e4487276e85174ba04756f8fe5253fbdd
Jelius Basumatary <personal@jelius.dev>
2026-03-11 20:19:26 +0530

    Improved schema and added some documentations and fixme/todo notes to fix/implement some stuff later

--- types/schema.go
+++ types/schema.go
@@ -7,42 +7,99 @@ import (
     "github.com/jelius-sama/logger"
 )
 
-// Sakuhin is designed with Manga and Doujin in mind, it can probably represent other work such as LN too.
+// NOTE: For people wondering about use of `panic` in String() methods, read the middleware package, it already
+// handles server panics gracefully by restarting the server and sending a 500 error to the client. While this
+// may not be the best way to handle this sort of errors in a critical high volume server but this is supposed to
+// be a local server and so panicing will leave logs full of red color (with the help of the custom logger) and
+// will be easier to debug and find issues early. Just make sure to do all the DB ops in transaction to avoid any
+// potential data loss if the server was processing something with the database so that rollbacks are possible.
+
+// TODO: Make two schema type structures, one represents data before or at database level and one respresents
+// data after leaving database package which is to be used by the entire application. Also note that data
+// validation will take place in database layer with sql statements designed to prevent incorrect data from
+// even entering the database in the first place so that we don't need an app layer validator.
+// Schema before or at DB layer: `db.Sakuhin.PrequelID - uuid.UUID`.
+// Schema after DB layer, to be used by the app: `types.Sakuhin.Prequel - types.Sakuhin`
+// Basically foreign key will be replaced by the row that the key points to so that we have the data ready to use.
+
+// TODO: There are some parts that says uuid.UUID but those should be the struct type instead, it's only uuid in
+// the database, after it croses the DB package layer it is not gonna be uuid but rather the whole table which
+// would be in the struct type of that field, example include `Sakuhin.PrequelID`, `Sakuhin.SequelID`, etc.
+
+// Sakuhin is designed with Manga and Doujin in mind, it can probably loosely represent other work such as LN too.
 type Sakuhin struct {
-    ID                uuid.UUID            `json:"id"`
-    Title             StringWithLang       `json:"title"`
-    Description       StringWithLang       `json:"description"`
-    CoverArts         []string             `json:"cover_arts"`
-    Tags              SakuhinTag           `json:"tags"`
-    Category          SakuhinCategory      `json:"category"`
-    Demographic       *SakuhinDemographic  `json:"demographic"` // can be nil if unspecified demographic or multi-demographic
-    ContentRating     SakuhinContentRating `json:"content_rating"`
-    Artist            []string             `json:"artist"`
-    Author            []string             `json:"author"`
-    Character         SakuhinCharacter     `json:"character"`
-    Language          string               `json:"language"`
-    SeriesID          uuid.UUID            `json:"series_id"` // All installments/parts of a series share this same ID
-    PrequelID         *[]uuid.UUID         `json:"prequel_id"`
-    SequelID          *[]uuid.UUID         `json:"sequel_id"`
-    Parodies          *[]SakuhinParodies   `json:"parodies"` // if nil then original series, this is mostly applicable to doujin
-    PageCount         uint64               `json:"page_count"`
-    InstallmentNumber uint64               `json:"installment_number"`
-    DisplayLabel      DisplayLabel         `json:"display_label"`
-    ReleasedAt        time.Time            `json:"released_at"`
-    UploadedAt        time.Time            `json:"uploaded_at"`
-    UpdatedAt         time.Time            `json:"updated_at"`
+    // ID is primary key of a row that is used to uniquely identify it.
+    ID uuid.UUID `json:"id"`
+    // Title is the title of work the row represents in two supported language, note that I do not plan on adding
+    // support for any new language as I made this service with my own use case in mind and I'm proficient in English
+    // and am currently learning Japanese.
+    Title StringWithLang `json:"title"`
+    // Description is the description of the work, I don't think this needs much explanation.
+    Description StringWithLang `json:"description"`
+    // CoverArts will store the front page of the work in case it is provided, if not provided we use the first page
+    // of the work. I will use an unique ID in a very specific directory where all the cover arts will be stored, this
+    // way we can change the path to directory any time we want and use the ID to get the file within that specific
+    // directory.
+    CoverArts *[]uuid.UUID `json:"cover_arts"`
+    // Tag is probably gonna be a new table with a foreign key to the row it represents, it contains a bunch of tags.
+    Tags SakuhinTag `json:"tags"`
+    // I'm not sure if category will come in any use but let's just keep it just in case to distinguish.
+    Category SakuhinCategory `json:"category"`
+    // Demographic is the target audience base of a specific work. Can be nil in some cases.
+    Demographic   *SakuhinDemographic  `json:"demographic"`    // can be nil if unspecified demographic or multi-demographic
+    ContentRating SakuhinContentRating `json:"content_rating"` // self explanatory
+    Artist        []SakuhinCreator     `json:"artist"`         // self explanatory
+    Author        []SakuhinCreator     `json:"author"`         // self explanatory
+    // All the recognizable character in the work, this is probably also a new table with a foreign key to the row it
+    // represents.
+    Character SakuhinCharacter `json:"character"`
+    // Language of the work, for most cases it's gonna be en or jp but there are some doujin that are only available
+    // online in language like cn or kr and is often left without translation so I made language a generic string,
+    // it would be better if I can change this to a enum later in the future, note that the value must be the
+    // language name in full, example: "english", "japanese", etc.
+    Language string `json:"language"`
+    // ID of a specific series, this is unique only between different series and a series with multiple volumes, cour,
+    // parts, etc. regardless of how it is being released/serialized, would have the same SeriesID.
+    SeriesID uuid.UUID `json:"series_id"` // All installments/parts of a series share this same ID
+    // IDs of the row that is prequel to this installment. Note that while the field/property says prequel, it is not
+    // prequel in the sense of storyline/timeline but rather the release date instead.
+    PrequelID *[]uuid.UUID `json:"prequel_id"`
+    // Same as previously defined field PrequelID just for Sequels and as mentioned previously, it is also based on
+    // release date rather than the timeline of the series as there are certain series such as fate where it is hard to
+    // say which specific installment is prequel and which installment is sequel due to it sheer amount of references to
+    // different series and the sheer number of installments that it currently has.
+    SequelID *[]uuid.UUID `json:"sequel_id"`
+    // Parodies is almost certainly dedicated to doujin and is pretty self explanatory.
+    Parodies  *[]SakuhinParodies `json:"parodies"`   // if nil then the series is an original series
+    PageCount uint16             `json:"page_count"` // self explanatory
+    // Seriel number of the installment release count starting from 1..n installments.
+    InstallmentNumber uint32 `json:"installment_number"` // Is there any series with more than 4 billion installments?
+    // This field was mostly for the frontend but I guess we can use it to see if the installment is a "special", "extra", etc.
+    DisplayLabel DisplayLabel `json:"display_label"`
+    ReleasedAt   time.Time    `json:"released_at"` // self explanatory
+    UploadedAt   time.Time    `json:"uploaded_at"` // self explanatory
+    UpdatedAt    time.Time    `json:"updated_at"`  // self explanatory
 }
 
+// Modern language don't have unions so I'm just gonna enforce in the frontend that a title must be provided, either En or Jp.
 type StringWithLang struct {
-    English  string `json:"english"`
-    Japanese string `json:"japanese"`
+    English  *string `json:"english"`
+    Japanese *string `json:"japanese"`
 }
 
 type SakuhinTag struct {
     // TODO: Probably better to have a enum dedicated to themes and genres defining all possible combinations
-    Themes  []string  `json:"themes"`
-    Genres  []string  `json:"genres"`
-    Circles *[]string `json:"circles"` // Circle is nil for manga and list of circles/groups for doujin
+    Themes  []string  `json:"themes"`  // Themes are stuff like "foot fetish", "tomboy", "gore", "ugly bastard", "slasher", etc.
+    Genres  []string  `json:"genres"`  // Genres are stuff like "Horror", "Romance", "Yuri", "Isekai", "Comedy", "Action", etc.
+    Circles *[]string `json:"circles"` // Circle is most likely gonna be nil for manga and list of circles/groups for doujin.
+}
+
+// Role agnostic strucutre which can be used to represent either an artist, author, etc.
+// This is probably gonna be a table which is autogenerated whena new author/artist name is referenced while uploading a work.
+// This way we can add autocomplete feature displaying the name when the user types while uploading new works in the frontend UI.
+type SakuhinCreator struct {
+    ID   uuid.UUID      `json:"id"`
+    Name StringWithLang `json:"name"`
 }
 
 type SakuhinCategory uint32
@@ -51,7 +108,7 @@ type SakuhinContentRating uint32
 
 const (
     CategoryDoujin SakuhinCategory = iota
-    CategorySakuhin
+    CategoryManga
 )
 
 const (
@@ -62,18 +119,18 @@ const (
 )
 
 const (
-    RatingSafe       SakuhinContentRating = iota
-    RatingErotica                         // Soft pornographic work (blured censorship, holy light censorship, etc.)
-    RatingSuggestive                      // Not pornographic work
-    RatingNSFW                            // Uncensored pornographic work
+    RatingSafe       SakuhinContentRating = iota // SFW Work
+    RatingSuggestive                             // Not pornographic work but not SFW either
+    RatingErotica                                // Soft pornographic work (blured censorship, holy light censorship, etc.)
+    RatingNSFW                                   // Uncensored pornographic work
 )
 
 func (mc SakuhinCategory) String() string {
     switch mc {
     case CategoryDoujin:
         return "Doujin"
-    case CategorySakuhin:
-        return "Sakuhin"
+    case CategoryManga:
+        return "Manga"
     }
 
     logger.Panic("unreachable")
@@ -175,16 +232,20 @@ type SakuhinCharacter struct {
     SupportingCharacters []ACharacter `json:"supporting_characters"`
     SideCharacters       []ACharacter `json:"side_characters"`
     Antagonists          []ACharacter `json:"antagonists"`
+    Unknown              []ACharacter `json:"unknown"`
 }
 
+// NOTE: THIS HAS TO BE A TABLE OF CHARACTER WITH EACH CHARACTER HAVING AN UNIQUE ID.
 type ACharacter struct {
-    EnglishTitle  *string   `json:"english_title"`  // if nil, then character name is probably not translated
-    JapaneseTitle *string   `json:"japanese_title"` // if nil, then character is probably not of japanese origin
-    Photos        *[]string `json:"photos"`         // if nil, then the character probably is a mythical faceless character or just not been revealed in the story yet
+    EnglishTitle  *string      `json:"english_title"`  // if nil, then character name is probably not translated
+    JapaneseTitle *string      `json:"japanese_title"` // if nil, then character is probably not of japanese origin
+    Photos        *[]uuid.UUID `json:"photos"`         // if nil, then the character probably is a mythical faceless character or just not been revealed in the story yet
 }
 
 type SakuhinParodies struct {
-    OriginalID uuid.UUID    `json:"original_id"`
+    // TODO: Update this shit, I cannot guarentee that I would have the original series downloaded
+    // that the doujin parodies (I'm not sure if parodies is the right word here, maybe parody-ed).
+    OriginalID uuid.UUID    `json:"original_id"` // FIXME: Foreign key to the original series
     Characters []ACharacter `json:"characters"`
 }
 
@@ -194,7 +255,7 @@ type SakuhinPublicationStat struct {
     InitialRelease  time.Time `json:"initial_release"`
     LatestRelease   time.Time `json:"latest_release"`
     CurrentStatus   Status    `json:"status"`
-    ReleaseSchedule Schedule  `json:"Schedule"`
+    ReleaseSchedule Schedule  `json:"schedule"`
 }
 
 type Status uint32
